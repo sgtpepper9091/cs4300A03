d91fadc77c93fee9a9a24afafa6c31f0db7f002e
Alberto Briceno <acbriceno@live.com>
2018-10-23 17:01:20 -0230

    Update Physics.cpp
    Physics Overlap on Bounding Boxes

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -4,6 +4,12 @@
 Vec2 Physics::GetOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b)
 {
     // TODO: Returns the overlap rectangle between the bounding boxes of entities a and b
-
-    return Vec2(0, 0);
+    
+    Vec2 *delta = new Vec2((abs((a->getComponent<CTransform>()->pos.x)  -  (b->getComponent<CTransform>()->pos.x)))
+                           ,(abs((a->getComponent<CTransform>()->pos.y) -  (b->getComponent<CTransform>()->pos.y))));
+    
+    float ox = (a->getComponent<CBoundingBox>()->halfSize.x) + (b->getComponent<CBoundingBox>()->halfSize.x) - delta->x;
+    float oy = (a->getComponent<CBoundingBox>()->halfSize.y) + (b->getComponent<CBoundingBox>()->halfSize.y) - delta->y;
+    
+    return Vec2(ox, oy);
 }
